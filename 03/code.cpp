477ff2db298555a6a4bd96407aa4d117317818f4
MarekPelowski <marek.pelowski@proton.me>
2023-11-21 16:34:57 +0100

    make the code work with input of every number

--- 03/code.cpp
+++ 03/code.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
+#include <math.h>
 
 int main()
 {
     int n;
     std::cin >> n;
-
-    int arr[3];
-
+    
+    int size = 0;
+    int *arr = NULL;
     int division = 1;
 
-    for(int i = 0; i < 3; i++){
+
+    for(int i = 1; i <= n; i*=10){
+        if(n / i != 0){
+            size++;
+        }
+    }
+
+    arr = new int[size];
+
+    division = 1;
+
+    for(int i = 0; i < size; i++){
         int modulus = n % (division * 10);
         modulus = modulus / division;
     
@@ -18,11 +30,10 @@ int main()
         division *= 10;
     }
 
-    division = 100;
+    division = pow(10, size - 1);
     int num = 0;
 
-
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < size; i++){
         arr[i] *= division;
         num += arr[i];
 
