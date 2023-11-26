073d2560e51484b4b1ad332059de41283d91dc67
MarekPelowski <marek.pelowski@protonmail.com>
2023-11-26 19:17:55 +0100

    add NONE text if there are no perfect nums

--- 08/code.cpp
+++ 08/code.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 bool isPerfectNum(int N);
 
@@ -6,11 +7,17 @@ int main()
 {
     int a, b;
     std::cin >> a >> b;
+    bool findPerfectNum = false;
 
     for(int i = a; i <= b; i++){
         if(isPerfectNum(i) == 1){
             std::cout << i << std::endl;
+            findPerfectNum = true;
         }
+    }
+
+    if(findPerfectNum == false){
+        std::cout << "NONE";
     }
 
     return 0;
