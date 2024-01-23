5e26867a05f49e77be118efd7ed03b69bb1fd329
MarekPelowski <marek.pelowski@protonmail.com>
2024-01-23 19:03:21 +0100

    finish a exercise about balanced texts

---
+++ 25/code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    std::string S;
+    std::cin >> S;
+
+    int countA = 0;
+    int countB = 0;
+
+    for(int i = 0; i < S.length(); i++){
+        if(S[i] == 'A' || S[i] == 'a') {
+            countA++;
+        } else if (S[i] == 'B' || S[i] == 'b') {
+            countB++;
+        }
+    }
+
+    if(countA == countB && countA > 0){
+        std::cout << "YES";
+    } else {
+        std::cout << "NO";
+    }
+
+    return 0;
+}
