4aed9c66fb801dba4ef4f03437a9e24b7a5246c6
MarekPelowski <marek.pelowski@protonmail.com>
2024-02-24 22:23:58 +0100

    leave the exercise for now (I will  come back later)

--- course/01/akc/akc.cpp
+++ course/01/akc/akc.cpp
@@ -5,13 +5,11 @@ int findParticleSpeed(int particle, std::vector<int> &particles)
 {   
     int bottomRange = 0;
     int topRange = particles.size() - 1; // the last index
-
-    int bottomParticle, topParticle;
+    int bottomParticle, topParticle; 
 
     // use binary search to find the index of the first particle
     while(bottomRange < topRange) {
         int middle = (bottomRange + topRange) / 2;
-
         if(particles[middle] >= particle)
             topRange = middle;
         else
@@ -19,22 +17,19 @@ int findParticleSpeed(int particle, std::vector<int> &particles)
     }
     bottomParticle = bottomRange; // save the first particle index
 
+    if(particles[bottomParticle] != particle)
+        return 0;
 
     // use binary search again to find the index of the last particle
     topRange = particles.size() - 1;
     while(bottomRange < topRange) {
         int middle = (bottomRange + topRange + 1) / 2;
-
         if(particles[middle] <= particle)
             bottomRange = middle;
         else
             topRange = middle - 1;
     }
     topParticle = topRange;
-
-    if (bottomParticle >= particles.size() || particles[bottomParticle] != particle) 
-        return 0;
-
 
     return (topParticle - bottomParticle) + 1;
 }
@@ -43,7 +38,6 @@ int main()
 {
     int n;
     std::cin >> n; // number of particles
-
     std::vector<int> particles(n); // particles speed
     for(int i = 0; i < n; i++){
         std::cin >> particles[i]; 
@@ -51,12 +45,10 @@ int main()
 
     int q;
     std::cin >> q; // number of inquiries
-
     std::vector<int> results; // variable that contains the results
     for(int i = 0; i < q; i++){
         int particle;
         std::cin >> particle; // the particle speed to be found
-
         results.push_back(findParticleSpeed(particle, particles));
     }
 
