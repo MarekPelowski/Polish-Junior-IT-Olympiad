760f18239ccbc2eac60d7c738f9994ff650a7c77
MarekPelowski <marek.pelowski@protonmail.com>
2024-03-17 10:22:26 +0100

    finish the exercise

--- stage 1/bal/bal.cpp
+++ stage 1/bal/bal.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cmath>
+
+long long solve(long long n)
+{
+    // Rm - Radius smallest (amount of pail for the smallest circle radius)
+    long long Rs = n / 14;
+
+    // Radius Bajometr smallest (the height of the radius of the smallest circle)
+    long long RBs = std::sqrt(Rs);
+
+    // the radius of the smallest circle is 1/12 of whole snowman so we multiply it by 12
+    long long result = RBs * 12;
+
+    return result;
+}
 
 int main()
 {
-    int n;
+    long long n;
     std::cin >> n;
+
+    std::cout << solve(n);
 
     return 0;
 }
