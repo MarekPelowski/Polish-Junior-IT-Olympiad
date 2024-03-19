f237a68929cd0ea97fcbdcb2da23f7631cccca45
MarekPelowski <marek.pelowski@protonmail.com>
2024-03-19 21:05:10 +0100

    finish the exercise

--- stage 1/oij/oij.cpp
+++ stage 1/oij/oij.cpp
@@ -5,7 +5,8 @@ std::string solve(long long n)
     std::string text;
 
     int biggestDivisor = n;
-    
+    int biggestDivisor2 = n;
+
     // n
     if(n < 5998){
         for(int i = 0; i < n; i++)
@@ -20,7 +21,7 @@ std::string solve(long long n)
         if(n % i == 0)
             biggestDivisor = i;
     
-    if(biggestDivisor * n / biggestDivisor < 5999){
+    if(biggestDivisor + n / biggestDivisor < 5999){
         for(int i = 0; i < biggestDivisor; i++)
             text += 'O';
         for(int i = 0; i < n / biggestDivisor; i++)
@@ -36,11 +37,27 @@ std::string solve(long long n)
         if(n % i == 0)
             biggestDivisor = i;
 
-    for(int i = 2; i * i * i <= n; i++)
+    for(int i = 2; i * i <= biggestDivisor * (n / biggestDivisor); i++)
         if(n % i == 0)
-            biggestDivisor = i;
+            biggestDivisor2 = i;
 
-    return text;
+
+    int biggestDivisor3 = ((n / biggestDivisor) / biggestDivisor2);
+
+    if(biggestDivisor + biggestDivisor2 + biggestDivisor3 < 6000){
+        for(int i = 0; i < biggestDivisor; i++)
+            text += 'O';
+            
+        for(int i = 0; i < biggestDivisor2; i++)
+            text += 'I';
+
+        for(int i = 0; i < biggestDivisor3; i++)
+            text += 'J';
+
+        return text;
+    }
+
+    return 0;
 }
 
 int main()
--- stage 1/oij/tempCodeRunnerFile.cpp
+++ stage 1/oij/tempCodeRunnerFile.cpp
@@ -1,1 +1,13 @@
+    // for(int i = 2; i * i <= n; i++)
+    //     if(n % i == 0)
+    //         biggestDivisor = i;
+    
+    // if(biggestDivisor + n / biggestDivisor < 5999){
+    //     for(int i = 0; i < biggestDivisor; i++)
+    //         text += 'O';
+    //     for(int i = 0; i < n / biggestDivisor; i++)
+    //         text += 'I';
+
+    //     text += "J";
+    //     return text;
     // }
