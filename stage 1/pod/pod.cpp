f655a5ad8b189bb43778236b030f18f68bc6618f
MarekPelowski <marek.pelowski@protonmail.com>
2024-03-22 15:35:33 +0100

    I'm not able to do this for now. I will come back later

---
+++ stage 1/pod/pod.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    // I'm not able to do this for now. I will come back later.
+
+    return 0;
+}
---
+++ stage 1/pod/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+        // for (int d = 0; d <= 9; d++) {
+        //     std::cout << d << std::endl;
+        // //     if (i % pr[d + 1] >= pr[d]) {
+        // //             for (int mod = 0; mod < k; mod++) {
+        // //             int new_mod = (10 * mod + d) % k;
+        // //             DP[i - pr[d]][new_mod] += DP[i][mod];
+        // //         }
+        // //     }
+        // }
