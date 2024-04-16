51585d280a7db09d8158835c20187743e7903e59
MarekPelowski <marek.pelowski@protonmail.com>
2024-04-16 21:08:52 +0200

    make the chess exercise

--- stage 1/kro/kro.cpp
+++ stage 1/kro/kro.cpp
@@ -11,35 +11,38 @@ int findPart(int x1, int x2, int y1, int y2) {
         else return 4;
 }
 
-int changeToNumbers(char letter) {
-    return (letter - 'a') + 1;
+std::string findDiagonal(int part){
+    if(part == 1) return "NW";
+    if(part == 2) return "NE";
+    if(part == 3) return "SW";
+    if(part == 4) return "SE";
+    return 0;
 }
 
 int findPath(int part, std::vector<std::string> &moves, int x1, int x2, int y1, int y2) {
     int count = 0;
 
-    if(part == 1){
-        while(x1 > x2 && y1 < y2) {
-            moves.push_back("NW");
-            x1--;
-            y1++;
+    while(x1 != x2 && y1 != y2) {
+        moves.push_back(findDiagonal(part));
+        if(part == 1){ x1--; y1++; }
+        else if(part == 2){ x1++; y1++; }
+        else if(part == 3){ x1--; y1--; }
+        else if(part == 4){ x1++; y1--; }
+
+        count++;
+    }
+    if(y1 == y2) {
+        while(x1 != x2) {
             count++;
+            if(part == 1 || part == 3) { x1--; moves.push_back("W"); }
+            else if (part == 2 || part == 4) { x1++; moves.push_back("E"); }
         }
-        if(x1 == x2 || y1 == y2) {
-            return count;
-        } else if (x1 == x2)
-            while(y1 < y2) {
-                moves.push_back("N");
-                y1++;
-                count++;
-            }
-        else {
-            while(x1 > x2) {
-                moves.push_back("E");
-                x1++;
-                count++;
-            }
-        }
+    } else if (x1 == x2) {
+        while(y1 != y2) {
+            count++;
+            if(part == 1 || part == 2) { y1++; moves.push_back("N"); }
+            else if (part == 3 || part == 4) { y1--; moves.push_back("S"); }
+        } 
     }
 
     return count;
@@ -54,8 +57,8 @@ int main()
     std::cin >> STRxy1;
     std::cin >> STRxy2;
 
-    x1 = changeToNumbers(STRxy1[0]);
-    x2 = changeToNumbers(STRxy2[0]);
+    x1 = (STRxy1[0] - 'a') + 1;
+    x2 = (STRxy2[0] - 'a') + 1;
     y1 = (STRxy1[1] - '1') + 1;
     y2 = (STRxy2[1] - '1') + 1;
 
@@ -65,5 +68,6 @@ int main()
     for(int i = 0; i < moves.size(); i++){
         std::cout << moves[i] << " ";
     }
+
     return 0;
 }
