257e1b1b827effc0791783dcc82af9922470b500
MarekPelowski <marek.pelowski@protonmail.com>
2024-05-30 12:13:03 +0200

    commit a not finished version

--- stage 1/mno/mno.cpp
+++ stage 1/mno/mno.cpp
@@ -13,38 +13,38 @@ int main()
     }
     std::sort(A.begin(), A.end());
     std::reverse(A.begin(), A.end());
-    for(int i = 0; i < N; i++) {
-        std::cout << A[i] << " ";
-    }
-    std::cout << std::endl;
-    int num;
-    int amount = 1;
-    std::vector<int> nums;
+    long long num = 1;
+    std::vector<long long> nums;
+    std::vector<long long> smallnums;
 
     for(int i = 0; i < N; i++) {
         if(T % A[i] == 0) {
-            num = A[i];
             nums.erase(nums.begin(), nums.end());
+            num = A[i];
             nums.push_back(num);
-            for(int j = i + 1; j < N; j++) {
-                if(num * A[j] <= T) {
-                    num *= A[j];
-                    amount++;
-                    nums.push_back(A[j]);
-                }
-
-                if(num == T){
-                    std::cout << i << " " << amount << " nums: ";
-                    for(int k = 0; k < nums.size(); k++) {
-                        std::cout << nums[k] << " ";
+            for(int j = i; j < N; j++) {
+                if(j + 1 < N) {
+                    if(num * A[j + 1] <= T) {
+                        num *= A[j + 1];
+                        nums.push_back(A[j + 1]);
                     }
-                    std::cout << std::endl << std::endl;
-                    nums.erase(nums.begin(), nums.end());
+                }
+                if(num == T){
+                    if(smallnums.empty() or nums.size() < smallnums.size()) smallnums = nums;
                     num = A[i];
+                    nums.erase(nums.begin(), nums.end());
                     nums.push_back(num);
-                    amount = 1;
                 }
             }
+        }
+    }
+    if(smallnums.empty()){
+        std::cout << "NIE";
+    }
+    else {
+        std::cout << smallnums.size() << std::endl;
+        for(int i = 0; i < smallnums.size(); i++) {
+            std::cout << smallnums[i] << " ";
         }
     }
 
