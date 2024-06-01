9167d3a723a027a77d7bd2b732d9eb8f0cffec34
MarekPelowski <marek.pelowski@protonmail.com>
2024-06-01 20:55:41 +0200

    save the progress (not finished yet)

--- stage 1/mno/mno.cpp
+++ stage 1/mno/mno.cpp
@@ -14,39 +14,25 @@ int main()
     std::sort(A.begin(), A.end());
     std::reverse(A.begin(), A.end());
     long long num = 1;
-    std::vector<long long> nums;
+    std::vector<long long> divisors;
     std::vector<long long> smallnums;
 
     for(int i = 0; i < N; i++) {
+        std::cout << A[i] << " ";
+    }
+    std::cout << "\n\n";
+
+    for(int i = 0; i < N; i++) {
         if(T % A[i] == 0) {
-            nums.erase(nums.begin(), nums.end());
-            num = A[i];
-            nums.push_back(num);
-            for(int j = i; j < N; j++) {
-                if(j + 1 < N) {
-                    if(num * A[j + 1] <= T) {
-                        num *= A[j + 1];
-                        nums.push_back(A[j + 1]);
-                    }
-                }
-                if(num == T){
-                    if(smallnums.empty() or nums.size() < smallnums.size()) smallnums = nums;
-                    num = A[i];
-                    nums.erase(nums.begin(), nums.end());
-                    nums.push_back(num);
-                }
-            }
+            divisors.push_back(A[i]);
         }
     }
-    if(smallnums.empty()){
-        std::cout << "NIE";
-    }
-    else {
-        std::cout << smallnums.size() << std::endl;
-        for(int i = 0; i < smallnums.size(); i++) {
-            std::cout << smallnums[i] << " ";
-        }
+
+    for(int i = 0; i < divisors.size(); i++) {
+        std::cout << divisors[i] << " ";
     }
 
     return 0;
 }
+
+//not finished yet :)
