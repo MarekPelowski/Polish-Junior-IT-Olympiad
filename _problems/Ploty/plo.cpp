b2e756555422b85733cd57e902b224e3cd5322d4
Marek Pelowski <marek.pelowski@protonmail.com>
2024-12-01 19:16:03 +0100

    finished the problem for now (I'll come back later)

--- _problems/Ploty/plo.cpp
+++ _problems/Ploty/plo.cpp
@@ -51,61 +51,70 @@ int getIndex(vector<int> T, int value) {
 
 int main()
 {
-    int N;
-    cin >> N;
-    vector<pair<int, int>> T;
-    vector<pair<int, int>> singleT(N-2);
-    for (int i = 0; i < N - 2; i++) {
-        int p, q;
-        cin >> p >> q;
-        T.push_back(make_pair(p, q));
-        T.push_back(make_pair(q, p));
-        singleT[i] = make_pair(p, q);
-    }
-    sort(T.begin(), T.end());
-    vector<int> firsts, seconds;
-
-    int T_size = 2 * (N - 2);
-
-    for(int i = 0; i < T_size; i++) {
-        firsts.push_back(T[i].first);
-        seconds.push_back(T[i].second);
-    }
-
-    auto maxTable = getMax(seconds);
-    auto minTable = getMin(seconds);
-
-    for(int i = 0; i < N - 2; i++) {
-        int start = min(singleT[i].first, singleT[i].second) + 1;
-        int end = max(singleT[i].first, singleT[i].second) - 1;
-
-        int endIndex = getIndex(firsts, end);
-        int startIndex = getIndex(firsts, start - 1) + 1;
-
-        if(endIndex < startIndex)
-            continue;
-
-        int k = log2(end - start + 1);
-        int minNum = min(minTable[k][startIndex], minTable[k][endIndex - pow(2, k) + 1]);
-        int maxNum = max(maxTable[k][startIndex], maxTable[k][endIndex - pow(2, k) + 1]);
-
-        if(minNum < start - 1 || maxNum > end + 1) {
-            int wrongCount = 0;
-            pair<int, int> wrong;
-
-            for(int i = startIndex; i <= endIndex; i++) {
-                if(seconds[i] < start - 1 || seconds[i] > end + 1) {
-                    wrongCount++;
-                    wrong = make_pair(min(firsts[i], seconds[i]), max(firsts[i], seconds[i]));
-                }
-            }
-
-            if(wrongCount == 1)
-                cout << wrong.first << " " << wrong.second;
-            else
-                cout << start - 1 << " " << end + 1;
-            break;
+    int Z;
+    cin >> Z;
+    vector<vector<pair<int, int>>> T(Z), singleT(Z);
+    for(int i = 0; i < Z; i++) {
+        int N;
+        cin >> N;
+        for (int j = 0; j < N - 2; j++) {
+            int p, q;
+            cin >> p >> q;
+            T[i].push_back(make_pair(p, q));
+            T[i].push_back(make_pair(q, p));
+            singleT[i].push_back(make_pair(p, q));
         }
+    }
+
+    for(int i = 0; i < Z; i++) {
+        int N = singleT[i].size();
+        sort(T[i].begin(), T[i].end());
+        vector<int> firsts, seconds;
+
+        int T_size = 2 * N;
+
+        for(int j = 0; j < T_size; j++) {
+            firsts.push_back(T[i][j].first);
+            seconds.push_back(T[i][j].second);
+        }
+
+        auto maxTable = getMax(seconds);
+        auto minTable = getMin(seconds);
+
+        for(int j = 0; j < N; j++) {
+            int start = min(singleT[i][j].first, singleT[i][j].second) + 1;
+            int end = max(singleT[i][j].first, singleT[i][j].second) - 1;
+
+            int endIndex = getIndex(firsts, end);
+            int startIndex = getIndex(firsts, start - 1) + 1;
+
+            if(endIndex < startIndex)
+                continue;
+
+            int k = log2(end - start + 1);
+            int minNum = min(minTable[k][startIndex], minTable[k][endIndex - pow(2, k) + 1]);
+            int maxNum = max(maxTable[k][startIndex], maxTable[k][endIndex - pow(2, k) + 1]);
+
+            if(minNum < start - 1 || maxNum > end + 1) {
+                int wrongCount = 0;
+                pair<int, int> wrong;
+
+                for(int j = startIndex; j <= endIndex; j++) {
+                    if(seconds[j] < start - 1 || seconds[j] > end + 1) {
+                        wrongCount++;
+                        wrong = make_pair(min(firsts[j], seconds[j]), max(firsts[j], seconds[j]));
+                    }
+                }
+
+                if(wrongCount == 1)
+                    cout << wrong.first << " " << wrong.second;
+                else
+                    cout << start - 1 << " " << end + 1;
+                cout << "\n";
+                break;
+            }
+        }
+
     }
 
     return 0;
