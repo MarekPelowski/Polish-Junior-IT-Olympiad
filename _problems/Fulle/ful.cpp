fdbe52916a5084e7d8b0e822662bf00bfa99ca79
Marek Pelowski <marek.pelowski@protonmail.com>
2024-12-27 21:42:42 +0100

    solved the problem for 91%

--- _problems/Fulle/ful.cpp
+++ _problems/Fulle/ful.cpp
@@ -1,26 +1,51 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
   string A;
   cin >> A;
-  int counter[256] = {};
-  bool find2 = false, find3 = false;
-  for(int x : A)
-    counter[x]++;
+  vector <int> counter(256);
+  int threes = 0, twos = 0, bonus = 0, bonus2 = 0;
 
-  for(int x : counter) {
-    if(x == 2) find2 = true;
-    else if(x == 3) find3 = true; 
+  for(int x : A)
+    counter[x]++; 
+
+  for(int i = 0; i < (int)counter.size(); i++) {
+    threes += counter[i] / 3;
+    twos += counter[i] % 3 / 2;
+    if(counter[i] % 3 % 2 == 1) {
+      bonus++;
+      bonus2 += (counter[i] / 3 - 1) / 2;
+    } else 
+      bonus2 += counter[i] / 3 / 2;
   }
 
-  if(find2 && find3)
-    cout << 1 << "\n";
-  else
-    cout << 0 << "\n";
+  if(twos >= threes) {
+    cout << threes << "\n";
+    return 0;
+  }
+
+  int deletes = 0;
+  while(twos < threes) {
+    threes--;
+    twos++;
+    if(bonus > 0) {
+      twos++;
+      bonus--;
+    } else {
+      deletes++;
+      if(deletes % 2 == 0 && bonus2 > 0) {
+        twos++;
+        bonus2--;
+      }
+    }
+  }
+
+  cout << threes << "\n";
 
   return 0;
 }
