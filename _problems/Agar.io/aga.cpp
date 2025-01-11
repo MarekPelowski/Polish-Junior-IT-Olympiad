d7a9269b7a2f63f1d075b9e16463bab95a3e831d
Marek Pelowski <marek.pelowski@protonmail.com>
2025-01-11 18:54:17 +0100

    solved the problem for 100% now

--- _problems/Agar.io/aga.cpp
+++ _problems/Agar.io/aga.cpp
@@ -4,23 +4,6 @@
 
 using namespace std;
 
-int getBiggest(vector<long long> a, long long b) {
-  int left = 0, right = a.size() - 1;
-
-  while(left < right) {
-    int mid = (left + right + 1) / 2;
-
-    if(a[mid] >= b)
-      right = mid - 1;
-    else
-      left = mid;
-  }
-
-  if(a[left] >= b)
-    return -1;
-  return left;
-}
-
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
@@ -33,20 +16,24 @@ int main() {
     cin >> a[i];
     max_mass = max(max_mass, a[i]);
   }
-  sort(a.begin(), a.end());
 
-  int ans = 0;
+  sort(a.begin(), a.end());
+  vector<long long> available;
   long long mass = 2;
+  int ans = 0, big = 0;
 
   while(mass < max_mass) {
-    long long biggest = getBiggest(a, mass);
-    if(biggest == -1) {
+    while(a[big] < mass) {
+      available.push_back(a[big]);
+      big++;
+    }
+    if(available.size() == 0) {
       cout << "NIE\n";
       return 0;
     }
 
-    mass += a[biggest];
-    a.erase(a.begin() + biggest);
+    mass += available[available.size() - 1];
+    available.pop_back();
     ans++;
   }
 
