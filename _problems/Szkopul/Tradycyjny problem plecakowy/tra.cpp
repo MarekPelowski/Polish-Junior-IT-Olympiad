e9fe67374f9ca69334fd6039fa7d1b1d87d2c7e1
Marek Pelowski <marek.pelowski@protonmail.com>
2025-02-11 13:17:59 +0100

    solved Tradycyjny problem plecakowy

--- _problems/Szkopul/Tradycyjny problem plecakowy/tra.cpp
+++ _problems/Szkopul/Tradycyjny problem plecakowy/tra.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -6,11 +8,32 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	int N, K;
-	cin >> N >> K;
-	vector<int> A(N);
+	int n, k;
+	cin >> n >> k;
+	vector<pair<int, int>> a(n); // {w, u}
+	for(int i = 0; i < n; i++) {
+		cin >> a[i].first >> a[i].second;
+	}
 	
-	return 0
+	vector<long long> dp(k+1, -1);
+	dp[0] = 0;
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = k; j >= 0; j--) {
+			if(dp[j] != -1 && j+a[i].first <= k) {
+				dp[j+a[i].first] = max(dp[j+a[i].first], dp[j]+a[i].second);
+			}
+		}
+	}
+	
+	long long ans = 0;
+	for(int i = 0; i <= k; i++) {
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans << "\n";
+
+	return 0;
 }
 
 // https://szkopul.edu.pl/problemset/problem/MYTUWkTbMYi5EWkVhLLupYg2/site/?key=statement
