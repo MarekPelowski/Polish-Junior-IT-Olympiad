0e1fa8ebe178f1165de0849977550d2842310a55
Marek Pelowski <marek.pelowski@protonmail.com>
2025-03-22 20:25:24 +0100

    a stable version (without starting the dp)

--- _problems/OIG_3/Domek/dom.cpp
+++ _problems/OIG_3/Domek/dom.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+const int INF = 1e9+5;
+
 int n, k;
 vector<int> tree;
+vector<int> depth;
+vector<vector<int>> dp;
 int L, R;
 
 void dfs(int a) {
 	if(L <= a && a <= R) {
-		cout << a << " ";
+		for(int i = 1; i <= 20; i++) {
+			dp[a][i] = tree[a];
+		}
 		return;
 	}
 	
 	dfs(2*a);
 	dfs(2*a+1);
+	
+	
+}
+
+void dfs2(int a) {
+	if(L <= a && a <= R) {
+		depth[a] = 0;
+		return;
+	}
+	dfs2(2*a);
+	dfs2(2*a+1);
+	depth[a] = depth[2*a]+1;
 }
 
 int main() {
@@ -27,6 +46,8 @@ int main() {
 	L = (1 << (n-1));
 	R = (1 << n) - 1;
 	tree.resize(R+1);
+	depth.resize(R+1);
+	dp.resize(R+1, vector<int>(21));
 	
 	for(int i = 1; i <= R; i++) {
 		int a, b;
@@ -34,7 +55,16 @@ int main() {
 		tree[i] = a+b;
 	}
 	
+	dfs2(1);
 	dfs(1);
+	
+	for(int i = 1; i <= R; i++) {
+		cout << i << ":\n";
+		for(int j = 0; j <= 20; j++) {
+			cout << dp[i][j] << " ";
+		}
+		cout << "\n\n";
+	}
 	
 	
 	return 0;
