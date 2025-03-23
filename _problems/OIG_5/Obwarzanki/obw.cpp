66608ce347023951e3a73e76d8114ba70397cbb9
Marek Pelowski <marek.pelowski@protonmail.com>
2025-03-23 13:56:18 +0100

    solved Obwarzanki for the second time

--- _problems/OIG_5/Obwarzanki/obw.cpp
+++ _problems/OIG_5/Obwarzanki/obw.cpp
@@ -1,49 +1,47 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-const int INF = 1e9+5;
+int mxN = 1e6+5;
+int n, m;
+vector<int> w(mxN), z(mxN);
+
+int solve(int inc) {
+	int pos = m+inc;
+	int minW = w[m], maxZ = z[m];
+	
+	int cnt = 0;
+	while(pos >= 0 && pos < n) {
+		if(z[pos] <= minW || w[pos] >= maxZ) {
+			cnt++;
+		}
+		else {
+			minW = min(minW, w[pos]);
+			maxZ = max(maxZ, z[pos]);
+		}
+
+		pos+=inc;
+	}
+	
+	return cnt;
+}
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-
-  int n, m;
-  cin >> n >> m;
-  vector<int> z(n), w(n);
-  for(int i = 0; i < n; i++) {
-    cin >> w[i] >> z[i];
-  }
-
-  m--;
-  int minW = w[m], maxZ = z[m];
-  int cnt = 0;
-
-  for(int i = m-1; i >= 0; i--) {
-    if(maxZ <= w[i] || z[i] <= minW) {
-      cnt++;
-    } else {
-      minW = min(minW, w[i]);
-      maxZ = max(maxZ, z[i]);
-    }
-  }
-
-  int ans1 = m - cnt;
-  minW = w[m], maxZ = z[m];
-  cnt = 0;
-
-  for(int i = m+1; i < n; i++) {
-    if(maxZ <= w[i] || z[i] <= minW) {
-      cnt++;
-    } else {
-      minW = min(minW, w[i]);
-      maxZ = max(maxZ, z[i]);
-    }
-  }
-  
-  int ans2 = n - m - 1 - cnt;
-
-  cout << min(ans1, ans2) << "\n";
-
-  return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	cin >> n >> m;
+	m--;
+	for(int i = 0; i < n; i++) {
+		cin >> w[i] >> z[i];
+	}
+	
+	int ans1 = m - solve(-1);
+	int ans2 = n-m-1 - solve(1);
+	
+	cout << min(ans1, ans2) << "\n";
+	
+	return 0;
 }
