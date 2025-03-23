90f49e1de64bcb61c8925b275c285ad4e8536476
Marek Pelowski <marek.pelowski@protonmail.com>
2025-03-23 19:21:50 +0100

    solved Drzewa fully

--- _problems/OIG_8/Drzewa/drz.cpp
+++ _problems/OIG_8/Drzewa/drz.cpp
@@ -1,56 +1,91 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
 
-#define ll long long
 using namespace std;
+
+int n;
+string s;
+
+char type(int i, int b) {
+	char c = '?';
+	if(s[i] == 'J') {
+		if(b == 0) c = 'J';
+		else if(b == 1) c = 'S';
+	}
+	else {
+		if(b == 1) c = 'J';
+		else if(b == 2) c = 'S';
+	}
+	
+	return c;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	
-	int n;
-	cin >> n;
-	string s;
-	cin >> s;
-	
-	vector<int> pref(n+1);
-	unordered_map<int, vector<int>> suf;
-	int stat = 0;
+	cin >> n >> s;
+	vector<int> pref(n);
+	int bal = 0;
 	for(int i = 0; i < n; i++) {
-		if(s[i] == 'J') stat++;
-		else stat--;
-		pref[i+1] = stat;
-	}
-	stat = 0;
-	for(int i = n-1; i > 0; i--) {
-		if(s[i] == 'J') stat++;
-		else stat--;
-		suf[stat].push_back(i-1);
+		if(s[i] == 'J') bal++;
+		else bal--;
+		pref[i] = bal;
 	}
 	
-	int A = n, B = n;
+	unordered_map<int,int> sufJ, sufS;
+	for(int i = 0; i < n; i++) {
+		int suf = pref[n-1] - pref[i];
+		if(s[i] == 'J') sufJ[suf] = i;
+		else sufS[suf] = i;
+	}
+	
+	int p = pref[n-1];
+	int a, b;
+
+	for(int inc=-2, i=0; inc<=2; inc+=2, i++) {
+		if((p+inc)%3 == 0) {
+			a = (p+inc)/3;
+			b = i;
+		}
+	}
+	
+	int A = -1, B = -1;
 	
 	for(int i = 1; i <= n-4; i++) {
-		int x = pref[i];
-		for(int j : suf[x]) {
-			if(j > i+1 && j <= n-2) {
-				int seg = pref[j] - pref[i+1];
-				if(seg == x) {
-					A = i+1;
-					B = min(B, j+1);
+		if(pref[i-1] == a) {
+			char c = type(i, b);
+			if(c == '?') continue;
+			
+			int suf;
+			if(c == 'J') suf = sufJ[a];
+			else suf = sufS[a];
+			
+			bool flag = false;
+			if(i+1 < suf) {
+				for(int j = i+2; j <= n-2; j++) {
+					if(s[j] == c) {
+						if(pref[n-1] - pref[j] == a) {
+							A = i+1;
+							B = j+1;
+							flag = true;
+							break;
+						}
+					}
 				}
 			}
-		}
-		
-		if(A == i+1) {
-			cout << A << " " << B << "\n";
-			return 0;
+			if(flag) break;
 		}
 	}
 	
-	cout << "BRAK\n";
+	if(A == -1) {
+		cout << "BRAK\n";
+		return 0;
+	}
+	
+	cout << A << " " << B << "\n";
 	
 	return 0;
 }
