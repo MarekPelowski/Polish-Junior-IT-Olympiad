1fac1e07abadb7943148f862fb8e9739cf9eab16
Marek Pelowski <marek.pelowski@protonmail.com>
2025-04-03 16:41:46 +0200

    solved Zarowki

--- _problems/OIG_9/Zarowki/zar.cpp
+++ _problems/OIG_9/Zarowki/zar.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int n, m;
 string s;
 
-int solve() {
+int ones(int L, int R) {
 	int cnt = 0;
-	for(int i = 0; i < n; i++) {
-		if(s[i] == '1' && (i == n-1 || s[i+1] == '0')) {
+	for (int i = L; i <= R; i++) {
+		if (i < 0 || i >= n) continue;
+		if (s[i] == '1' && (i == R || i == n-1 || s[i+1] == '0')) {
 			cnt++;
 		}
 	}
-	cnt*=2;
-	if(s[0] == '1') cnt--;
 	return cnt;
+}
+
+int output(int cnt) {
+	return (s[0] == '1') ? 2*cnt-1 : 2*cnt;
 }
 
 int main() {
@@ -23,12 +25,19 @@ int main() {
 	
 	cin >> n >> m >> s;
 	
-	cout << solve() << "\n";
-	while(m--) {
+	int cnt = ones(0, n-1);
+	cout << output(cnt) << "\n";
+	
+	for (int i = 0; i < m; i++) {
 		int x;
 		cin >> x;
-		s[x-1] = (s[x-1] == '1') ? '0' : '1';
-		cout << solve() << "\n";
+		x--;
+		int onesOld = ones(x-1, x+1);
+		s[x] = (s[x] == '1') ? '0' : '1';
+		int onesNew = ones(x-1, x+1);
+		
+		cnt += onesNew - onesOld;
+		cout << output(cnt) << "\n";
 	}
 	
 	return 0;
